31a7f25052988418db3d24d65fe0364913b5ff0a
noonchaa <enouchiha11@gmail.com>
2021-06-15 03:53:27 +0700

    slug again

--- components/blog/Post.js
+++ components/blog/Post.js
@@ -2,8 +2,6 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { BLOCKS } from '@contentful/rich-text-types';
 
 export default function Post({post}) {
-    const tanggal = new Date(post.sys.createdAt)
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
     const CustomRender = ({url}) => (
         <div>
@@ -26,7 +24,7 @@ export default function Post({post}) {
                     <h2 className="text-xs text-blue-500 tracking-widest font-medium title-font mb-1 uppercase text-center">moto</h2>
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 capitalize">{post.fields.judul}</h1>
                     <p className="lg:w-2/3 leading-relaxed text-base pb-5">
-                    {tanggal.toLocaleDateString('ID',options)}
+                    tanggal
                     </p>
                     <div className='prose max-w-none lg:prose-lg xl:prose-xl'>
                         {documentToReactComponents(post.fields.post,optionsRender)}
