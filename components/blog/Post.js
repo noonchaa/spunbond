e5186007c1abd247b51eb9fcdb67ab68aa9fcb6c
noonchaa <enouchiha11@gmail.com>
2021-06-15 04:14:10 +0700

    still debug slug

--- components/blog/Post.js
+++ components/blog/Post.js
@@ -3,31 +3,17 @@ import { BLOCKS } from '@contentful/rich-text-types';
 
 export default function Post({post}) {
 
-    const CustomRender = ({url}) => (
-        <div>
-            <img src={`https:${url}`} alt='post' width='auto' height='auto' className='mx-auto'/>
-        </div>
-    )
-    const optionsRender = {
-        renderNode:{
-            [BLOCKS.EMBEDDED_ASSET] : (node) => {
-                const {url} = node.data.target.fields.file
-                return <CustomRender url={url} />
-            }
-        }
-    }
-
     return(
         <section className='text-gray-600 body-font'>
             <div className="container px-5 py-12 mx-auto">
                 <div className="flex flex-col w-full">
                     <h2 className="text-xs text-blue-500 tracking-widest font-medium title-font mb-1 uppercase text-center">moto</h2>
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 capitalize">{post.fields.judul}</h1>
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 capitalize">judul</h1>
                     <p className="lg:w-2/3 leading-relaxed text-base pb-5">
                     tanggal
                     </p>
                     <div className='prose max-w-none lg:prose-lg xl:prose-xl'>
-                        {documentToReactComponents(post.fields.post,optionsRender)}
+                        {documentToReactComponents(post.fields.post)}
                     </div>
                 </div>
             </div>
