38d6671bd1229dbd44a782e9157379cfeb667363
noonchaa <enouchiha11@gmail.com>
2021-06-15 16:05:29 +0700

    acces sys

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,4 +1,6 @@
 import {createClient} from 'contentful'
+import Layout from '../../components/Layout'
+import Header from '../../components/blog/Header'
 
 const client = createClient({
     space: process.env.CONTENTFUL_ID,
@@ -25,7 +27,7 @@ export const getStaticProps = async ({params}) => {
     return {
         props : {
             postDetail : items[0],
-            home : homepage[0],
+            home : homepage.items[0],
             sales: salesman.items.map(item => ({...item.fields}))
         },
         revalidate: 10
@@ -34,11 +36,29 @@ export const getStaticProps = async ({params}) => {
 
 const PostDetail = ({postDetail,home, sales}) => {
     if (!postDetail) return <h1>...loading...</h1>
-    const {judul} = postDetail.fields
+    const {judul,keyword,post} = postDetail.fields
+    const {createdAt} = postDetail.sys
+    const {
+        title,
+        text1,
+        text2,
+        headOffice,
+        factory,
+        heroImage,
+        facebook,
+        instagram,
+        twitter,
+        linkedIn,
+        motto
+    } = home.fields
+
     return (
-        <div>
-            <h1>{judul}</h1>
-        </div>
+        <Layout title={judul} desc={keyword} foottitle={title} lead={text1} second={text2} 
+        factory={factory} office={headOffice} img={heroImage.fields.file.url} fb={facebook} 
+        twit={twitter}insta={instagram} lin={linkedIn} sales={sales}>
+            <Header link={['profile','aplikasi','contact']} titleLink={'/'} img={heroImage.fields.file.url} />
+
+        </Layout>
     )
 }
 
