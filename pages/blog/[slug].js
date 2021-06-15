a2860b334c579ff9fc04a97296ec5a7d72f6f9e8
noonchaa <enouchiha11@gmail.com>
2021-06-15 13:52:24 +0700

    debug 2

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,7 +1,4 @@
-import Layout from "../../components/Layout";
 import {createClient} from 'contentful'
-import Header from "../../components/blog/Header";
-import Post from "../../components/blog/Post";
 
 const client = createClient({
     space: process.env.CONTENTFUL_ID,
@@ -10,9 +7,10 @@ const client = createClient({
 
 export const getStaticPaths = async () => {
     const res = await client.getEntries({content_type:'blogPost'})
-    const paths = res.items.map(item => {
+    const data = await res.items
+    const paths = data.map(item => {
         return {
-            params:{slug: item.fields.slug}
+            params: {slug: item.fields.slug}
         }
     })
     return {
@@ -20,31 +18,34 @@ export const getStaticPaths = async () => {
         fallback: true
     }
 }
-
 export const getStaticProps = async ({params}) => {
-  const {items} = await client.getEntries({content_type:'blogPost','fields.slug':params.slug})
-  const homepage = await client.getEntries({content_type:'homePage', 'fields.siteTitle': 'Care Spunbond'})
-  const salesman = await client.getEntries({content_type: 'salesman'})
-  return {
-    props: {
-      isipost: items[0],
-      homePage: homepage.items[0],
-      salesman: salesman.items.map(item=>({...item.fields})),
-    },
-    revalidate: 60
-  }
+    const {items} = await client.getEntries({content_type:'blogPost','fields.slug':params.slug})
+    if (!items.length){
+        return {
+            redirect:{
+                destination:'/blog',
+                permanent: false
+            }
+        }
+    }
+    return {
+        props:{
+            isiPost:items[0]
+        },
+        revalidate:10
+    }
 }
 
-export default function Posts({isipost,homePage,salesman}) {
-    const {siteTitle,description,title,text1,text2,headOffice,factory,heroImage,facebook,instagram,
-            twitter,linkedIn} = homePage.fields
+const Posts = ({isiPost}) => {
+    const {judul, keyword,post} = isiPost.fields
+    const {createdAt} = isiPost.sys
 
     return(
-        <Layout title={siteTitle} desc={description} foottitle={title} lead={text1} second={text2} factory={factory} 
-        office={headOffice} img={heroImage.fields.file.url} fb={facebook} twit={twitter}insta={instagram} 
-        lin={linkedIn} sales={salesman} >
-            <Header link={['profile','aplikasi','contact']} titleLink={'/'} img={heroImage.fields.file.url} />
-            <Post blogPost={isipost} />
-        </Layout>
+        <div>
+        <h1>{judul}</h1>
+        <h1>{keyword}</h1>
+        <h1>{createdAt}</h1>
+        </div>
     )
 }
+export default Posts
